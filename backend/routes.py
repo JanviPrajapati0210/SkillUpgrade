0f294e4a7ae84b0821a33f2020e804c7bc98dc6a
JanviPrajapati0210 <24aiml052@charusat.edu.in>
2026-01-25 11:29:06 +0530

    Added API routes for backend

--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from auth import register_user, login_user
+from skill_analysis import analyze_skills
 
 api = Blueprint("api", __name__)
 
-@api.route("/test")
+@api.route("/test", methods=["GET"])
 def test():
     return {"message": "Routes working"}
+
+@api.route("/register", methods=["POST"])
+def register():
+    data = request.json
+    return register_user(data)
+
+@api.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    return login_user(data)
+
+@api.route("/analyze-skills", methods=["POST"])
+def analyze():
+    data = request.json
+    return jsonify(analyze_skills(data))
